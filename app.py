2d0463c6e297da30f2aecfdabfe9e1e5beed7bce
alexfat <alexfat@mail.ru>
2021-09-05 13:15:00 +0600

    hmmm

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+# 29-08-21
 from flask import Flask, render_template, request, make_response
 
-number = ''
+number = ''  # основная переменная для хранения выражения(экрана калькулятора)
+actions = ['+', '-', '*', '/']
 app = Flask(__name__)
+
 
 def calculating(number):
     """операции вычисления"""
@@ -11,30 +14,44 @@ def calculating(number):
         if number[-1] in actions:  # если выражение вида только 'число' и 'действие'
             number = number + number[:-1]
 
-        if '.' in number:  # TODO сделать вычисления для двух чисел с точкой
+        if '.' in number:  # вычисления для чисел с точкой
             for action in actions:
                 if action in number:
                     number = number.split(action)
-                    number = str(float(number[0]) + float(number[1]))
-
-        for action in actions:
-            if action in number:
-                number = number.split(action)
-                if action == '+':
-                    number = str(int(number[0]) + int(number[1]))
-                elif action == '-':
-                    number = str(int(number[0]) - int(number[1]))
-                elif action == '*':
-                    number = str(int(number[0]) * int(number[1]))
-                elif action == '/':
-                    if number[1] == '0':
-                        number = 'ERROR'
-                    else:
-                        number = str(int(number[0]) / int(number[1]))
+                    if action == '+':
+                        number = str(float(number[0]) + float(number[1]))
+                    elif action == '-':
+                        number = str(float(number[0]) - float(number[1]))
+                    elif action == '*':
+                        number = str(float(number[0]) * float(number[1]))
+                    elif action == '/':
+                        if number[1] == '0':
+                            number = 'ERROR'
+                        else:
+                            number = str(float(number[0]) / float(number[1]))
+            if number[-1] == '0' and number[-2] == '.':  # если после точки только ноль - вывод числа без дробной части
+                number = number[:-2]
+        else:  # вычисления для целых чисел
+            for action in actions:
+                if action in number:
+                    number = number.split(action)
+                    if action == '+':
+                        number = str(int(number[0]) + int(number[1]))
+                    elif action == '-':
+                        number = str(int(number[0]) - int(number[1]))
+                    elif action == '*':
+                        number = str(int(number[0]) * int(number[1]))
+                    elif action == '/':
+                        if number[1] == '0':
+                            number = 'ERROR'
+                        else:
+                            number = str(int(number[0]) / int(number[1]))
+                        if number[-1] == '0' and number[-2] == '.':  # после точки только ноль - вывод целого числа
+                            number = number[:-2]
     except:
         number = 'ERROR'
-
     return number
+
 
 def responser(result):
     resp = make_response(render_template('indicator.html', number=result))
@@ -51,7 +68,9 @@ def calculator():
 
 @app.route('/count', methods=['POST'])  # web application calculator
 def count():
+    """обработка нажимаемых кнопок"""
     global number
+    global actions
     data = request.form.get('number')
     number = request.cookies.get('calc_cook')
     if number == 'HELLO':
@@ -61,7 +80,7 @@ def count():
         return responser(number)
     elif data == '<':
         number = number[:-1]
-    elif data == '.':
+    elif data == '.':  # TODO доработать ввод чисел с точкой
         if '.' in number:
             return responser(number)
         else:
@@ -73,7 +92,7 @@ def count():
         resp = make_response(render_template('indicator.html', number=result))
         resp.set_cookie('calc_cook', number)
         return resp
-    elif data == '+' or data == '-' or data == '*' or data == '/':  #TODO обсчет длинных выражений
+    elif data == '+' or data == '-' or data == '*' or data == '/':  # TODO обсчет длинных выражений
         number = calculating(number)+data
         return responser(number)
     else:
